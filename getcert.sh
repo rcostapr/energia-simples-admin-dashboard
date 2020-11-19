493fb8f2f067b5a1e0e6bf1b62e4bf44bdcecbd6
rcostapr <ruicosta.pr@gmail.com>
2020-11-19 15:06:19 +0000

    Delete getcert.sh

--- getcert.sh
+++
@@ -1,4 +0,0 @@
-grep -A 27 -e '-----BEGIN PRIVATE KEY.*' /opt/psa/var/certificates/scfrW4AyS > ssl.key
-grep -m 1 -A 31 -e '-----BEGIN CERTIFICATE-----.*' /opt/psa/var/certificates/scfrW4AyS > ssl.cert
-cat /opt/psa/var/certificates/scfOAsWK7 > ssl.ca
-
